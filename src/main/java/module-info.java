a5d07e4e9282bb1298d45caac9fda7834b422d12
ViveKumar007 <prustyvivek2020@gmail.com>
2025-11-04 15:15:21 +0530

    stuff

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,11 +1,12 @@
 module com.example.demo1 {
     requires javafx.controls;
     requires javafx.fxml;
-    requires com.google.gson;
-    requires java.net.http;
-    requires java.sql;
+    requires javafx.graphics;
+    requires javafx.web;  
+    requires java.sql;   
 
-
+    
+    requires com.google.gson; 
     opens com.example.demo1 to javafx.fxml, com.google.gson;
     exports com.example.demo1;
 }
